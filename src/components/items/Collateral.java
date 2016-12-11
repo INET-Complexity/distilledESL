968c3f145eeaf4418ab87f93bb5e0a23931c9874
Rafa Baptista Ochoa <rafabap93@hotmail.com>
2016-12-11 21:45:40 +0000

    Adding Collateral

---
+++ src/components/items/Collateral.java
@@ -0,0 +1,10 @@
+package components.items;
+
+import ESL.inventory.Item;
+
+/**
+ * This interface allows a contract or good to be placed as collateral for a repo contract.
+ */
+public interface Collateral {
+    boolean encumbered=false;
+}
---
+++ src/components/items/Equity.java
@@ -0,0 +1,14 @@
+package components.items;
+
+import ESL.inventory.Good;
+
+public class Equity extends Good {
+    public Equity(Double amount) {
+        super("Equity",amount);
+    }
+
+    //TODO: PRICE OF EQUITY?
+    static GBP price;
+
+}
+
